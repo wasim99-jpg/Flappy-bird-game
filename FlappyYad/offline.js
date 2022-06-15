5943bff3ec161bfae6b594c0462da61dd1c9acac
Wasim <57243273+wasim99-jpg@users.noreply.github.com>
2022-06-15 12:31:26 +0800

    Add files via upload

---
+++ FlappyYad/offline.js
@@ -0,0 +1,21 @@
+﻿{
+	"version": 1655190093,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/background-sheet0.png",
+		"images/bird-sheet0.png",
+		"images/pipebottom-sheet0.png",
+		"images/pipetop-sheet0.png",
+		"images/tiledbackground.png",
+		"images/tiledbackground2.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
